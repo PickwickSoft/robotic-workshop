249a44c4ff6ed8703c6097e23de09fbc2f9e730a
Stefan Garlonta <stefan.garlonta@gmail.com>
2023-07-05 19:48:25 +0200

    Improve full turn constants

--- workshop/Mission_1/Data/constants.py
+++ workshop/Mission_1/Data/constants.py
@@ -1,5 +1,5 @@
 # You found the treasure! These are a few magical constants that can help you to control your car.
 
-FULL_TURN = 900
+FULL_TURN = 915
 HALF_TURN = FULL_TURN/2
 QUARTER_TURN = FULL_TURN/4
